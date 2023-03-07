dd5b4a619fba8a184e2ac2b9ee522ef081a81d3c
Samuel Hale <samuelhalebusiness@gmail.com>
2023-03-07 09:41:46 -0500

    Commit boiler plate files

---
+++ date.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "user.h"
+#include "date.h"
+int main(int argc, char *argv[])
+{
+  struct rtcdate r; // date.h contains this data structure
+  if (date(&r)) { // I believe this is a function we have to define as it is not contained in any of the header files
+    printf(2, "date failed\n");
+    exit();
+  }
+  // print the time as a formatted string
+  printf(1, "%d-%d-%d %d:%d:%d\n", 
+           r.month, r.day, r.year, r.hour, r.minute, r.second);
+  exit();
+}
